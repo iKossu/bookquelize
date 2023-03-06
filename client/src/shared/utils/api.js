f4742f4ea7807e5651cfba9af95f7aa3aa02d576
iKossu <ikossu@gmail.com>
2023-03-06 22:46:41 +0200

    Added axios & api util

---
+++ client/src/shared/utils/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export const apiUrlPrefix = process.env.REACT_APP_API_URL ?? 'http://localhost:5000';
+
+const api = axios.create({
+  baseURL: apiUrlPrefix,
+  timeout: 20000,
+});
+
+export const postRequest = async (url, data) => {
+  const response = await api.post(url, data);
+  return response;
+};
+
+export const getRequest = async (url) => {
+  const response = await api.get(url);
+  return response;
+};
