4335f43c1b8fe549a62305eb240701a54b9200dd
iKossu <ikossu@gmail.com>
2023-03-12 23:40:17 +0200

    Changed Books store

--- client/src/shared/stores/Books.js
+++ client/src/shared/stores/Books.js
@@ -1,14 +1,19 @@
 import { makeAutoObservable } from 'mobx';
 
 class Books {
+  bookCount = 0;
   books = [];
 
   constructor() {
     makeAutoObservable(this);
   }
 
+  setBookCount(bookCount) {
+    this.bookCount = bookCount;
+  }
+
   setBooks(books) {
-    this.books = books;
+    this.books = [...this.books, ...books];
   }
 }
 
